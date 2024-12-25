16f0762bbc92a2112d38a1e45f4b54fbde969cd4
AndriiKendzor <kendzorkv@gmail.com>
2024-12-25 16:07:46 +0100

    new-parser-line

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.action_chains import ActionChains
 from datetime import datetime
 import time
 
@@ -16,8 +17,8 @@ options.add_argument("--disable-gpu")
 options.add_argument("--no-sandbox")
 
 # Автоматичне завантаження та налаштування ChromeDriver
-service = Service(ChromeDriverManager().install())
-driver = webdriver.Chrome(service=service, options=options)
+options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36")
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
 
 
 # Функція для обробки ціни
@@ -31,6 +32,25 @@ def parse_price(price_str):
     else:
         return float(clean_str)
 
+def press_load_more_button():
+    load_more_button = WebDriverWait(driver, 5).until(
+        EC.element_to_be_clickable((By.XPATH, "//button[contains(@class, 'History_loadMore__IilgR')]"))
+    )
+    load_more_button.click()
+    print("Clicked 'Load More' button.")
+
+def find_token_address(action):
+    hover_element = action
+
+    actions = ActionChains(driver)
+    actions.move_to_element(hover_element).perform()
+
+    token_address = WebDriverWait(driver, 20).until(
+        EC.presence_of_element_located((By.XPATH, "//div[contains(@class, 'TransactionAction_addr')]"))
+    ).text
+
+    return token_address
+
 
 # Основна функція парсера транзакцій
 def get_data_from_user(address):
@@ -38,25 +58,74 @@ def get_data_from_user(address):
     driver.get(url)
 
     try:
-        # Очікуємо появу першої транзакції
-        first_transaction = WebDriverWait(driver, 20).until(
-            EC.presence_of_element_located((By.XPATH, "//div[contains(@class, 'History_tableLine')]"))
-        )
+        #finding first transaction
+        first_transaction = None
+        while not first_transaction:
+            try:
+                # Спроба знайти першу транзакцію, яка не містить клас 'History_error'
+                first_transaction = WebDriverWait(driver, 5).until(
+                    EC.presence_of_element_located((By.XPATH, "//div[contains(@class, 'History_tableLine') and not(contains(@class, 'History_error'))]"))
+                )
+            except:
+                # Якщо транзакція не знайдена, натискаємо кнопку 'Load More'
+                try:
+                    press_load_more_button()
+                    print("Clicked 'Load More' button.")
+                except Exception as e:
+                    print("No more transactions to load or error occurred:", e)
+                    break
 
-        # Збираємо дані транзакції
-        time_of_tx = first_transaction.find_element(By.XPATH, ".//div[contains(@class, 'History_sinceTime')]").text
-        amount = first_transaction.find_element(By.XPATH,
-                                                ".//span[contains(@class, 'ChangeTokenList_tokenPrice')]").text
-        token = first_transaction.find_element(By.XPATH, ".//span[contains(@class, 'ChangeTokenList_tokenName')]").text
+        # Перевіряємо кожен елемент окремо
+        try:
+            time_of_tx = first_transaction.find_element(By.XPATH, ".//div[contains(@class, 'History_sinceTime')]").text
+        except Exception:
+            time_of_tx = "NOT FOUND"
 
-        parsed_amount = parse_price(amount)
+        try:
+            amount = first_transaction.find_element(By.XPATH, ".//span[contains(@class, 'ChangeTokenList_tokenPrice')]").text
+        except Exception:
+            amount = "NOT FOUND"
 
-        if parsed_amount > 300:
-            message = f"\ud83d\udea8 High Transaction Alert \ud83d\udea8\nUSER: {address}\nToken: {token}\nAmount: {parsed_amount:.2f} $\nTime: {time_of_tx}"
-            send_message(message)
+        try:
+            token = first_transaction.find_element(By.XPATH, ".//span[contains(@class, 'ChangeTokenList_tokenName')]").text
+        except Exception:
+            token = "NOT FOUND"
 
-        print(f"Time: {time_of_tx}, Amount: {parsed_amount:.2f} $, Token: {token}")
-        print("-------------")
+        try:
+            action = first_transaction.find_element(By.XPATH, ".//div[contains(@class, 'TransactionAction_action')]")
+            action_text = action.text
+        except Exception:
+            action_text = "NOT FOUND"
+
+        try:
+            token_address = find_token_address(action)
+        except Exception:
+            token_address = "NOT FOUND"
+
+        #check amount
+        if amount == "NOT FOUND":
+            parsed_amount = "NOT FOUND"
+        else:
+            parsed_amount = parse_price(amount)
+
+            if parsed_amount > 1000:
+                message = f"\ud83d\udea8 High Transaction Alert \ud83d\udea8\n" \
+                          f"USER: {address}\n" \
+                          f"Time: {time_of_tx} \n" \
+                          f"Action: {action_text} \n" \
+                          f"Amount: {parsed_amount:.2f} $\n" \
+                          f"Token: {token}\n" \
+                          f"Token address: {token_address} \n"
+                send_message(message)
+
+        print(
+              f"Time: {time_of_tx} \n"
+              f"Action: {action_text} \n"
+              f"Amount: {parsed_amount:.2f} $\n"
+              f"Token: {token}\n"
+              f"Token address: {token_address}\n"
+              "-------------"
+              )
     except Exception as e:
         print(f"Error while parsing: {e}")
 
